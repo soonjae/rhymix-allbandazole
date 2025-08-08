c76a2109b694462d26e7d8bff702e14fa45a740a
Soonjae Kim <soonj@nate.com>
2025-08-08 11:56:01 +0900

    Update ko.php
    geoip 라벨 추가

--- lang/ko.php
+++ lang/ko.php
@@ -5,6 +5,7 @@ $lang->cmd_allbandazole_general_config = '기본 설정';
 $lang->cmd_allbandazole_enabled = '구충제 모듈 사용';
 $lang->cmd_allbandazole_user_agents = '차단할 User-Agent';
 $lang->cmd_allbandazole_ip_blocks = '차단할 IP 대역';
+$lang->cmd_allbandazole_geoip_blocks = '차단할 국가코드';
 $lang->msg_allbandazole_multiline = '한 줄에 하나씩 입력해 주세요.';
 $lang->msg_allbandazole_your_user_agent = '현재 접속자도 차단될 수 있는 설정입니다. User-Agent를 다시 확인해 주세요.';
 $lang->msg_allbandazole_your_ip_block = '현재 접속자도 차단될 수 있는 설정입니다. IP 대역을 다시 확인해 주세요.';
